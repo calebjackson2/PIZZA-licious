85e7aabde5e634f4a885f21e2bc6b1df7a529507
caleb jackson <jacksoncaleb740@gmail.com>
2025-11-13 14:14:39 -0500

    Pizza class refactored with Menuitem

--- src/main/java/com/pluralsight/Pizza.java
+++ src/main/java/com/pluralsight/Pizza.java
@@ -1,25 +1,30 @@
 package com.pluralsight;
 
+import java.awt.*;
+import java.util.ArrayList;
 import java.util.List;
 
-public class Pizza {
+public class Pizza extends MenuItem {
     private String size;
     private String crust;
     private boolean stuffedCrust;
+    private Sauce sauce;
     private List<Topping> toppings;
 
     public Pizza(String size, String crust, boolean stuffedCrust, Sauce sauce) {
+        super(size + " Pizza");
         this.size = size;
         this.crust = crust;
         this.stuffedCrust = stuffedCrust;
-        this.toppings = new List<>();
+        this.sauce = sauce;
+        this.toppings = new ArrayList<>();
     }
 
     public void addTopping(Topping topping) {
         toppings.add(topping);
     }
 
-    public double calculateCost() {
+    public double getPrice() {
         double basePrice = switch (size.toLowerCase()) {
             case "personal" -> 8.50;
             case "medium" -> 12.00;
@@ -27,19 +32,30 @@ public class Pizza {
             default -> 10.00;
         };
         if (stuffedCrust) basePrice += 2.00;
-        for (Topping topping : toppings) basePrice += topping.getPrice();
+
+        for (Topping topping : toppings) {
+            basePrice += topping.getPrice();
+        }
         return basePrice;
     }
-
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        sb.append(size).append(" Pizza (").append(crust).append(" crust");
+        sb.append(size)
+                .append(" Pizza (").append(crust).append(" crust");
         if (stuffedCrust) sb.append(", stuffed crust");
-        sb.append(")\nToppings: ");
-        for (Topping topping : toppings) {
-            sb.append(topping.getName()).append(", ");
+        sb.append(") with ").append(sauce.getName()).append(" sauce\nToppings: ");
+
+        if (toppings.isEmpty()) {
+            sb.append("none");
+        } else {
+            for (Topping topping : toppings) {
+                sb.append(topping.getName()).append(", ");
+            }
+            sb.setLength(sb.length() - 2); // remove trailing comma
         }
+
+        sb.append(" - $").append(String.format("%.2f", getPrice()));
         return sb.toString();
     }
 }
